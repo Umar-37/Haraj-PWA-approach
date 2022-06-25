ea2758d2ad0a3bdb6f771abbc945638a444ce277
Umar-37 <omaralhussan@outlook.com>
2022-06-25 18:39:52 +0300

    none

---
+++ src/components/CustomCard.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { Card, CardContent, CardHeader, Link } from "framework7-svelte";
+    export let img =
+        "https://mimg6cdn.haraj.com.sa/userfiles30/2022-6-22/684x912-1_-SzyDJoQBc22h7a.jpg";
+    export let content = "default content";
+    export let title = "Framework7 yayy";
+
+    function doThis(params) {
+        console.log("opend", params);
+    }
+</script>
+
+<!-- when user click the card -->
+<Card expandable onCardBeforeOpen={doThis}>
+    <CardContent padding={false}>
+        <div class="bg-color-blue" style="height:200px">
+            <CardHeader textColor="white" class="display-block">
+                {title}
+                <br />
+                <img alt="some img" src={img} width="90" height="90" />
+            </CardHeader>
+            <Link
+                cardClose
+                color="white"
+                class="card-opened-fade-in"
+                style="position: absolute; right: 15px; top: 15px"
+                iconF7="xmark_circle_fill"
+            />
+        </div>
+        <div class="card-content-padding">
+            <p>
+                {content}
+            </p>
+        </div>
+    </CardContent>
+</Card>
---
+++ src/pages/omar.svelte
@@ -0,0 +1,87 @@
+<script>
+    import {
+        Card,
+        CardContent,
+        CardHeader,
+        Link,
+        Page,
+        Navbar,
+        Block,
+        BlockTitle,
+        useStore,
+    } from "framework7-svelte";
+    import CustomCard from "./../components/CustomCard.svelte";
+    import store from "../js/haraj.js";
+
+    //     let users = useStore('users', (value) => users = value);
+    //     const loadUsers = () => {
+    //   store.dispatch('getUsers');
+    //   };
+    //   loadUsers()
+    //   console.log(users)
+    //console.log(store.state.products[0])
+    store.dispatch("getItems");
+    let items=[];
+    $: console.log("the $ sign says: ", items);
+        useStore(store,'gettItems',(value)=>{
+            items=[...items,...value]
+            console.log("inside suerStore",items)
+            console.log("inside suerStore",items)
+        })
+        
+
+    ///////////////////
+    function name(params) {
+        setTimeout(() => {
+            console.log("called", items);
+            console.log("called2", store.state.items);
+            // items = [
+            //     ...store.state.items,
+            //     { id: "new itemmmm", bodyTEXT: "from seet time out" },
+            //     { id: "new itemmmm", bodyTEXT: "from seet time out" },
+            //     { id: "new itemmmm", bodyTEXT: "from seet time out" },
+            // ];
+        }, 3000);
+    }
+    //////////k
+    console.log("from omar.svelte", items);
+    let url1 = "https://thumbcdn.haraj.com.sa/";
+    let url3 = "/90x90/webp";
+    name();
+</script>
+
+<Page>
+    <Navbar title="Omar page" backLink="Back" />
+    <BlockTitle>About My App</BlockTitle>
+    <Block strong>
+        <p>
+            Fugiat perspiciatis excepturi, soluta quod non ullam deleniti. Nobis
+            sint nemo consequuntur, fugiat. Eius perferendis animi autem
+            incidunt vel quod tenetur nostrum, voluptate omnis quasi quidem
+            illum consequuntur, a, quisquam.
+        </p>
+        <p>
+            Laudantium neque magnam vitae nemo quam commodi, in cum dolore
+            obcaecati laborum, excepturi harum, optio qui, consequuntur?
+            Obcaecati dolor sequi nesciunt culpa quia perspiciatis, reiciendis
+            ex debitis, ut tenetur alias.
+        </p>
+    </Block>
+    <Card
+        outline="true"
+        padding="false"
+        noShadow="true"
+        title="the header"
+        content="Laudantium neque magnam vitae nemo quam commodi, in cum dolore obcaecati laborum, excepturi harum, optio qui, simple datasimple datasimple datasimple datasimple datasimple datasimple datasimple datasimple 
+ datasimple datasimple datasimple datasimple datac"
+        footer="here the footer"
+    />
+
+    {#each items as item}
+        <CustomCard
+            title={item.title}
+            content={item.bodyTEXT}
+            img={url1 + item.thumbURL + url3}
+        />
+    {/each}
+</Page>
